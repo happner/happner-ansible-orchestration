da656336c92323768c68a1d953cf84a514bc1f85
Grant Pidwell <grantpidwell@gmail.com>
2016-12-23 19:35:34 +0200

    Creating temp file to write Happn cluster config to for each container. This will be injected into the container via the Docker file COPY command.

--- examples/happn-cluster/app/server.js
+++ examples/happn-cluster/app/server.js
@@ -4,45 +4,44 @@
 
 var HappnCluster = require('happn-cluster');
 
-// these environment variables are injected into each container by the Ansible/Docker build process
-var mongoCollection = process.env['MONGO_COLLECTION'];
-var mongoUrl = process.env['MONGO_URL'];
+//var config = JSON.parse(process.env['CLUSTER_CONFIG']);
 
-var members = process.env['CLUSTER_MEMBERS'];
+var fs = require('fs');
 
-for (var x = 0; x < members.length; x++) {
-    var current = members[x];
+fs.readFile('/cluster_config_complete.json', 'utf8', function (err, data) {
+    if (err) {
+        return console.log(err);
+    }
 
-    hosts.push(current.host + ':' + current.port);
-}
+    var config = JSON.parse(data);
 
-var config = JSON.parse(process.env['CLUSTER_CONFIG']);
+    // start the cluster
+    HappnCluster.create(config)
+        .then(function (server) {
 
-// start the cluster
-HappnCluster.create(config)
-    .then(function (server) {
+            console.log(server.services.orchestrator.peers);
 
-        console.log(server.services.orchestrator.peers);
-
-        server.services.orchestrator.on('peer/add', function (member) {
-            console.log('arriving peer\n', member);
-        });
-
-        server.services.orchestrator.on('peer/remove', function (member) {
-            console.log('departing peer\n', member);
-        });
-
-        process.on('SIGINT', function () {
-            server.stop(/*{kill: true, wait: 2000},*/ function () {
-                // if (seq == 9) {
-                //   console.log('kill', process.pid);
-                //   return;
-                // }
-                process.exit(0);
+            server.services.orchestrator.on('peer/add', function (member) {
+                console.log('arriving peer\n', member);
             });
+
+            server.services.orchestrator.on('peer/remove', function (member) {
+                console.log('departing peer\n', member);
+            });
+
+            process.on('SIGINT', function () {
+                server.stop(/*{kill: true, wait: 2000},*/ function () {
+                    // if (seq == 9) {
+                    //   console.log('kill', process.pid);
+                    //   return;
+                    // }
+                    process.exit(0);
+                });
+            })
         })
-    })
-    .catch(function (error) {
-        console.error('\n', error.stack ? error.stack : error);
-        //process.exit(1);
-    });
+        .catch(function (error) {
+            console.error('\n', error.stack ? error.stack : error);
+            //process.exit(1);
+        });
+});
+
