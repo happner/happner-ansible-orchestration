54e5f9a4200de4e93846f07fd3f69b4b2ddbc3c4
nomilous <nomilous@gmail.com>
2017-03-14 12:57:22 +0200

    server

--- examples/happner-cluster/app/server.js
+++ examples/happner-cluster/app/server.js
@@ -5,46 +5,9 @@
 var HappnerCluster = require('happner-cluster');
 var config = require('cluster_config');
 
-//var config = JSON.parse(process.env['CLUSTER_CONFIG']);
+HappnerCluster.create(config)
 
-var fs = require('fs');
-
-fs.readFile('cluster_config.json', 'utf8', function (err, data) {
-    if (err) {
-        return console.log(err);
-    }
-
-    var config = JSON.parse(data);
-
-    // start the cluster
-
-
-    //HappnCluster.create(config)
-    //    .then(function (server) {
-    //
-    //        console.log(server.services.orchestrator.peers);
-    //
-    //        server.services.orchestrator.on('peer/add', function (member) {
-    //            console.log('arriving peer\n', member);
-    //        });
-    //
-    //        server.services.orchestrator.on('peer/remove', function (member) {
-    //            console.log('departing peer\n', member);
-    //        });
-    //
-    //        process.on('SIGINT', function () {
-    //            server.stop(/*{kill: true, wait: 2000},*/ function () {
-    //                // if (seq == 9) {
-    //                //   console.log('kill', process.pid);
-    //                //   return;
-    //                // }
-    //                process.exit(0);
-    //            });
-    //        })
-    //    })
-    //    .catch(function (error) {
-    //        console.error('\n', error.stack ? error.stack : error);
-    //        //process.exit(1);
-    //    });
-});
-
+  .catch(function (error) {
+    console.error(error);
+    process.exit(1);
+  });
